c5ff766864adec37e5403b626599da41569cd908
josep.valero.casas-aljama <josep.valero.casas-aljama@estudiantat.upc.edu>
2024-11-29 14:35:02 +0100

    màscares casi acabades

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 //const { ipcRenderer } = require('electron');
+//import Tweakpane from 'https://cdn.jsdelivr.net/npm/tweakpane@3.0.0/dist/tweakpane.min.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'dark';
@@ -9,33 +10,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.getElementById('videoTrackCanvas');
     const context = canvas.getContext('2d');
     const frames = [];
-    const frameList = document.getElementById('frameList');
-    const maskSelectorContainer = document.getElementById('maskSelectorContainer');
+    //const maskSelectorContainer = document.getElementById('maskContainer');
     const frameImage = document.getElementById('frameImage');
     const context2 = frameImage.getContext('2d');
-    const dropdownButton = document.getElementById('dropdownButton');
+    const propagate_button = document.getElementById('propagate_button');
+    const controlsContainer = document.getElementById('framesContainer');
+    const maskContainer = document.getElementById('maskContainer');
+    const canvasResult = document.getElementById('resultImage');
+    const context3 = canvasResult.getContext('2d');
     var natural_width;
     var natural_height;
     var frameIndex = 0;
     var first = true;
     var masks;
+    var numMasks = 0;
     //var maskIndex = 0;
-    $(document).ready(() => {
-        $('.selectpicker').selectpicker({
-            dropupAuto: false
-        }); // Inicializa el bootstrap-select
-        
+
+    propagate_button.addEventListener('click', () => {
+        fetch('http://localhost:5000/propagate_segmentation', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ video_path: `../TFG-Editor-de-video/data/Videos`, image_path: `../TFG-Editor-de-video/data/Videos\\${frameIndex}.jpeg` })
+        })
+        .then(response => response.json())
     });
     
-    const updateFrameList = () => {
-        frameList.innerHTML = '';
+    const updateControls = () => {
+        console.log('Updating controls');
+        controlsContainer.innerHTML = ''; // Limpia los controles existentes
         frames.forEach((frame, index) => {
-            const option = document.createElement('option');
-            option.value = index;
-            option.textContent = `Frame ${index}`;
-            frameList.appendChild(option);
+            const frameDiv = document.createElement('div');
+            frameDiv.classList.add('control-item');
+            frameDiv.textContent = `Frame ${index}`;
+            frameDiv.addEventListener('click', () => {
+                sendFrameToServer(index);
+                showFrame(index);
+            });
+            controlsContainer.appendChild(frameDiv);
         });
-        $('.selectpicker').selectpicker('refresh'); // Refresca el select después de actualizarlo
     };
 
     const captureFrames = (filePath) => {
@@ -55,6 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
                    
                     frameIndex = 0;
                     if (tempVideo.currentTime >= tempVideo.duration) {
+                        console.log('Finished capturing frames');
                         resolve();
                         return;
                     }                    
@@ -114,18 +129,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (window.electron) {
         window.electron.receive('load-video', async (filePath) => {
+            const loadingSpinner = document.getElementById('loadingSpinner');
+            loadingSpinner.classList.add('show');
             await captureFrames(filePath);
-            updateFrameList();
+            loadingSpinner.classList.remove('show');
+            updateControls();
+
         });
     } else {
         console.error('Electron object is not available');
     }
-
-    frameList.addEventListener('change', (event) => {
-        const index = parseInt(event.target.value, 10);
-        sendFrameToServer(index);
-        showFrame(index);
-    });
 
     frameImage.addEventListener('click', () => {
         const rect = frameImage.getBoundingClientRect();
@@ -133,8 +146,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const scaleY = natural_height / rect.height;
         const x = (event.clientX - rect.left) * scaleX;
         const y = (event.clientY - rect.top) * scaleY;
-        var imagePath =  `../TFG-Editor-de-video/data/Videos\\` + frameIndex + '.jpeg';
-        data = {
+        const imagePath =  `../TFG-Editor-de-video/data/Videos\\` + frameIndex + '.jpeg';
+        const data = {
             x: x,
             y: y,
             image_path: imagePath
@@ -145,66 +158,74 @@ document.addEventListener('DOMContentLoaded', () => {
             'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            const url = URL.createObjectURL(blob);
-            resultImage.src = url;
-            //var image_path2 =   `../TFG-Editor-de-video/data/Videos\` + frameIndex + '.jpeg';
+        }).then(response => response.blob())
+          .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const img = new Image();
+                img.src = url;
+                img.onload = () => {
+                context2.clearRect(0, 0, frameImage.width, frameImage.height);
+                context2.drawImage(img, 0, 0, frameImage.width, frameImage.height);
+                };
                 get_masks(data.image_path);
-            })
-        .catch((error) => {
-            console.error('Error loading image:', error);
-        })
+                var numMaskArray = [numMasks];
+                let data2 = {
+                        image_path: imagePath,
+                        mask: numMaskArray
+                };
+        
+                apply_Mask(mode=false, data2);
+          })
+          .catch((error) => {
+              console.error('Error loading image:', error);
+          });
     })
 
-    dropdownButton.addEventListener('click', () => {
-        maskSelectorContainer.classList.toggle('show');
-    });
 
-    maskSelectorContainer.addEventListener('click', (event) => {
-        // Verifica si se hizo clic en un elemento de máscara
+    maskContainer.addEventListener('click', (event) => {
         const maskItem = event.target.closest('.mask-item');
-        if (!maskItem) return; // Si no se hizo clic en un elemento de máscara, salir
-
-        // Alternar la clase de selección
-        if(!maskItem.classList.contains('selected')) {
-            maskItem.classList.toggle('selected');
-            maskItem.style.backgroundColor = masks[maskItem.getAttribute('data-value')][0];
-            maskItem.style.color = '#e0e1dd';
+        if (!maskItem) return; 
+        
+        if(event.target.classList.contains('move-up')){
+            const prevItem = maskItem.previousElementSibling;
+            if (prevItem && prevItem.classList.contains('mask-item')) {
+                maskContainer.insertBefore(maskItem, prevItem);
+            }
         }
+
+        else if(event.target.classList.contains('move-down')) {
+            const nextItem = maskItem.nextElementSibling;
+            if (nextItem && nextItem.classList.contains('mask-item')) {
+                maskContainer.insertBefore(nextItem, maskItem);
+            }
+        }
+
         else {
-            maskItem.classList.remove('selected');
-            maskItem.style.backgroundColor = '#e0e1dd';
-            maskItem.style.color = '#5a5a5a';
+
+            // Alternar la clase de selección
+            if(!maskItem.classList.contains('selected')) {
+                maskItem.classList.add('selected');
+                maskItem.style.backgroundColor = masks[maskItem.getAttribute('data-value')][0];
+                maskItem.style.color = '#e0e1dd';
+            }
+            else {
+                maskItem.classList.remove('selected');
+                maskItem.style.backgroundColor = '';
+                maskItem.style.color = '';
+            }
+
+            // Recopilar todas las máscaras seleccionadas
+            const selectedMasks = Array.from(maskContainer.querySelectorAll('.mask-item.selected'))
+                .map(item => parseInt(item.getAttribute('data-value')));
+
+            // Preparar los datos para enviar a la API
+            const data = {
+                image_path: `../TFG-Editor-de-video/data/Videos\\${frameIndex}.jpeg`,
+                mask: selectedMasks
+            };
+            apply_Mask(mode=true, data);
+            
         }
-
-        // Recopilar todas las máscaras seleccionadas
-        const selectedMasks = Array.from(maskSelectorContainer.querySelectorAll('.mask-item.selected'))
-            .map(item => parseInt(item.getAttribute('data-value')));
-
-        // Preparar los datos para enviar a la API
-        const data = {
-            image_path: `../TFG-Editor-de-video/data/Videos\\${frameIndex}.jpeg`,
-            mask: selectedMasks
-        };
-
-        // Hacer la solicitud para aplicar las máscaras seleccionadas
-        fetch('http://localhost:5000/applyMask', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            const url = URL.createObjectURL(blob);
-            resultImage.src = url; // Mostrar la imagen actualizada
-        })
-        .catch((error) => {
-            console.error('Error al aplicar las máscaras:', error);
-        });
     });
 
 
@@ -212,14 +233,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const maskItem = document.createElement('div');
         maskItem.classList.add('mask-item');
         maskItem.setAttribute('data-value', value);
-
+    
         const itemText = document.createElement('span');
         itemText.textContent = text;
-
+    
+        const colorButton = document.createElement('button');
+        colorButton.classList.add('color-button');
+    
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('delete-button');
         deleteButton.textContent = 'X';
-
+    
         // Lógica para borrar el elemento
         deleteButton.addEventListener('click', (e) => {
             e.stopPropagation(); // Evitar que se cierre el menú al hacer clic
@@ -227,14 +251,74 @@ document.addEventListener('DOMContentLoaded', () => {
             deleteMask(value);
             // Aquí puedes manejar cualquier lógica adicional para borrar la máscara
         });
-
-        
+    
         maskItem.style.borderLeft = `4px solid ${color}`;
-
+    
         maskItem.appendChild(itemText);
+        maskItem.appendChild(colorButton);
         maskItem.appendChild(deleteButton);
-        maskSelectorContainer.appendChild(maskItem);
+    
+        maskContainer.appendChild(maskItem);
+    
+        // Crear Pickr para el control del color
+        const pickr = Pickr.create({
+            el: colorButton,
+            theme: 'classic', // or 'monolith', or 'nano'
+            default: color,
+            components: {
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+            // Input / output Options
+            interaction: {
+                rgba: true,
+                hex: true,
+                hsla: true,
+                hsva: true,
+                rgb: true,
+                hsv: true,
+                input: true,
+                save: true
+            }
+            }
+        });
+
+        pickr.on('save', (colorInstance) => {
+            const hexColor = colorInstance.toHEXA().toString();
+            const rgbaArray = colorInstance.toRGBA();
+            const rgbaArrayNormalized = rgbaArray.map((value, index) => index < 3 ? value / 255 : 0.6);
+
+            if (maskItem.classList.contains('selected')) {
+                maskItem.style.backgroundColor = hexColor;
+            } else {
+                maskItem.style.backgroundColor = '';
+            }
+
+            colorButton.style.backgroundColor = hexColor;
+
+            fetch('http://localhost:5000/color', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                image_path: `../TFG-Editor-de-video/data/Videos\\${frameIndex}.jpeg`,
+                mask_index: value,
+                color: rgbaArrayNormalized
+            })
+            })
+            .then(response => response.json())
+            .then(response => {
+                maskItem.remove();
+                masks[value][0] = hexColor;
+                addOption(value, `Máscara ${value + 1}`, hexColor);
+            })
+            .catch(error => console.error('Error updating color:', error));
+        }); 
     }
+    
+
 
     function get_masks(image_path) {
         fetch(`http://localhost:5000/mask?image_path=${image_path}`, {
@@ -246,7 +330,8 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             masks = data.masks;
-            maskSelectorContainer.innerHTML = '';
+            numMasks = masks.length;
+            maskContainer.innerHTML = '';
             masks.forEach((mask, index) => {
                 const color = mask[0]; // Obtener el color de la máscara
                 addOption(index, `Máscara ${index + 1}`, color);
@@ -280,4 +365,52 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error al hacer la solicitud de eliminación:', error);
         });
     }
+
+    function apply_Mask(mode, data) {
+        if(mode) {
+            fetch('http://localhost:5000/applyMask', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                const img = new Image();
+                img.src = url;
+                img.onload = () => {
+                    context2.clearRect(0, 0, frameImage.width, frameImage.height);
+                    context2.drawImage(img, 0, 0, frameImage.width, frameImage.height);
+                    URL.revokeObjectURL(url); // Limpia el objeto URL
+                };
+            })
+            .catch((error) => {
+                console.error('Error al aplicar las máscaras:', error);
+            });
+        }
+        fetch('http://localhost:5000/applyMaskBlack', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const img = new Image();
+            img.src = url;
+            img.onload = () => {
+                context3.clearRect(0, 0, frameImage.width, frameImage.height);
+                canvasResult.width = img.width;
+                canvasResult.height = img.height;
+                context3.drawImage(img, 0, 0, canvasResult.width, canvasResult.height);
+            };
+        })
+        .catch((error) => {
+            console.error('Error loading image:', error);
+        });
+    }
+        
 });
